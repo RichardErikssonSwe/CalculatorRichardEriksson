822086624dfba1733b7e65d1cbf9a0fb169ec24e
Richard Eriksson <richardeswe@hotmail.com>
2016-04-06 11:56:17 +0200

    Second commit; added add, sub, mul and div

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,5 +2,18 @@
 public class Calculator {
 	double a;
 	double b;
+	double result;
 	
+	public double addition(double a, double b){
+		return a+b;
+	}
+	public double subtraction(double a, double b){
+		return a-b;
+	}
+	public double multiplication(double a, double b){
+		return a*b;
+	}
+	public double division(double a, double b){
+		return a/b;
+	}
 }
